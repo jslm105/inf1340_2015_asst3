0bdeb757d205cbbfc3b5a4addff768dcd7d10983
HonourCode <jake.miller@mail.utoronto.ca>
2015-12-16 04:12:15 -0500

    Created new test case jsons
    Updated test_exercise2 to use new jsons

--- test_exercise2.py
+++ test_exercise2.py
@@ -26,3 +26,25 @@ def test_returning():
     assert decide("test_returning_citizen.json", "countries.json") ==\
         ["Accept", "Accept", "Quarantine"]
 
+def test_visitors_with_visas():
+    """
+    Visitors to KAN
+    """
+    assert decide("test_visitors_with_visas.json", "countries.json") ==\
+        ["Accept"]
+
+def test_incomplete_entries():
+    """
+    Travellers with one entry missing from all possible fields
+    """
+    assert decide("test_incomplete_entries.json", "countries.json") ==\
+        ["Reject", "Reject", "Reject", "Reject", "Reject", "Reject", \
+         "Reject", "Reject", "Reject", "Reject", "Reject", "Quarantine", \
+         "Reject", "Reject"]
+
+def test_improper_passports():
+    """
+    Travellers with passports with more or less characters or "_"
+    """
+    assert decide("test_improper_passports.json", "countries.json") ==\
+           ["Reject", "Reject", "Reject"]
